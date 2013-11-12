0b438ad7c87d3688e6ef611ec523fa357e1a19ff
Marcos Caceres <marcos@marcosc.com>
2013-11-12 18:02:01 +0000

    Fixed styling (using fixjsstyle)

--- respecConfig.js
+++ respecConfig.js
@@ -1,9 +1,9 @@
 var respecConfig = {
   // specification status (e.g. WD, LCWD, WG-NOTE, etc.). If in doubt use ED.
-  specStatus: "unofficial",
+  specStatus: 'unofficial',
 
   // the specification's short name, as in http://www.w3.org/TR/short-name/
-  shortName: "reqs-installable-webapps",
+  shortName: 'reqs-installable-webapps',
 
   // if your specification has a subtitle that goes below the main
   // formal title, define it here
@@ -31,44 +31,44 @@ var respecConfig = {
   // only "name" is required
   editors: [
     {
-      name: "Marcos Cáceres",
-      url: "http://marcosc.com",
-      mailto: "w3c@marcosc.com",
-      company: "Mozilla",
-      companyURL: "http://mozilla.com/"
-              },
+      name: 'Marcos Cáceres',
+      url: 'http://marcosc.com',
+      mailto: 'w3c@marcosc.com',
+      company: 'Mozilla',
+      companyURL: 'http://mozilla.com/'
+              }
           ],
 
   // name of the WG
-  wg: "Web and Mobile Interest Group (WebMob)",
+  wg: 'Web and Mobile Interest Group (WebMob)',
 
   // URI of the public WG page
-  wgURI: "http://www.w3.org/Mobile/IG/",
+  wgURI: 'http://www.w3.org/Mobile/IG/',
 
   // name (without the @w3c.org) of the public mailing to which comments are due
-  wgPublicList: "public-web-mobile",
+  wgPublicList: 'public-web-mobile',
 
   // URI of the patent status for this WG, for Rec-track documents
   // !!!! IMPORTANT !!!!
   // This is important for Rec-track documents, do not copy a patent URI from a random
   // document unless you know what you're doing. If in doubt ask your friendly neighbourhood
   // Team Contact.
-  wgPatentURI: "",
+  wgPatentURI: '',
   // !!!! IMPORTANT !!!! MAKE THE ABOVE BLINK IN YOUR HEAD,
-  issueBase: "https://github.com/w3c-webmob/installable-webapps/issues",
+  issueBase: 'https://github.com/w3c-webmob/installable-webapps/issues',
   otherLinks: [{
-    key: "Repositorty",
+    key: 'Repositorty',
     data: [
-      {
-        href: "https://github.com/w3c-webmob/installable-webapps"
-                                                        },
-      {
-        value: "issues",
-        href: "https://github.com/w3c-webmob/installable-webapps/issues"
-                                                        },
-      {
-        value: "Commit History.",
-        href: "https://github.com/w3c-webmob/installable-webapps/commits/gh-pages"
-                                                        }]
-                                        }]
+				{
+					href: 'https://github.com/w3c-webmob/installable-webapps'
+				},
+				{
+					value: 'issues',
+					href: 'https://github.com/w3c-webmob/installable-webapps/issues'
+				},
+				{
+					value: 'Commit History.',
+					href: 'https://github.com/w3c-webmob/installable-webapps/commits/gh-pages'
+					}]
+			}]
 };
